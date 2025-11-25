9a509efd290db9e99e0ebebe8a00a2deee84a106
Guille Montaña-Valverde <93223951+g-montana-valverde@users.noreply.github.com>
2025-11-25 17:58:02 +0100

    Update plotting.py

--- CharacterizationOfTrophicLevelsInAmyloidNegative/src/functions_pipeline/plotting.py
+++ CharacterizationOfTrophicLevelsInAmyloidNegative/src/functions_pipeline/plotting.py
@@ -1,3 +1,43 @@
+def plot_correlation_with_mean(x, y, x_mean, y_mean, x_label, y_label, out_path):
+	
+	x_array=x.flatten()
+	y_array=y.flatten()
+	
+	x_array = zscore(x_array)
+	y_array = zscore(y_array)
+	x_mean = zscore(x_mean)
+	y_mean = zscore(y_mean)
+
+	r, p = pearsonr(x_array,y_array)
+	
+	if p < 0.001:
+		p_str = "***"
+	elif p < 0.01:
+		p_str = "**"
+	elif p < 0.05:
+		p_str = "*"
+	else:
+		p_str = "n.s."
+	title = f"r{p_str} = {r:.2f}"
+	plt.figure(figsize=(8.5, 6))
+	plt.scatter(x_array, y_array, alpha=0.4, s=100, color="#229AC2")
+	plt.scatter(x_mean, y_mean, c=y_mean, cmap='jet', s=200)
+	
+	# Fit line using seaborn regplot
+	ax=sns.regplot(x=x_array, y=y_array, scatter=False, line_kws={'color':'red', 'linewidth':4})
+	plt.xlabel(f"{x_label} (z)", fontsize=38)
+	plt.ylabel(f"{y_label} (z)", fontsize=38)
+	plt.title(title, fontsize=40)
+	plt.xticks(fontsize=38)
+	plt.yticks(fontsize=38)
+	ax.yaxis.set_major_locator(MaxNLocator(nbins=4))
+	ax.xaxis.set_major_locator(MaxNLocator(nbins=4))
+	plt.axhline(0, color='gray', linestyle='--', linewidth=1)
+	plt.axvline(0, color='gray', linestyle='--', linewidth=1)
+	plt.tight_layout()
+	plt.savefig(out_path)
+	plt.close()
+	
 def radar_plot_net(zTL_sfn, lower_threshold, upper_threshold, out_path):
 	angles = np.linspace(0, 2 * np.pi, len(SFN_names), endpoint=False).tolist()
 	angles += angles[:1]  # close the circle
