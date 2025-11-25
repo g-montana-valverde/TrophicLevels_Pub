debbfd7519e69c8d99b9c675a3c2da6d86331788
Guille Montaña-Valverde <93223951+g-montana-valverde@users.noreply.github.com>
2025-11-25 17:16:46 +0100

    Refactor GEC metrics calculation for clarity

--- CharacterizationOfTrophicLevelsInAmyloidNegative/src/TrophicLevels/metrics.py
+++ CharacterizationOfTrophicLevelsInAmyloidNegative/src/TrophicLevels/metrics.py
@@ -1,52 +1,49 @@
 
 def run_GEC_metrics(GEC_ALL):
-	mat_path = work_dir + 'GEC_metrics_all_subjects.mat'
-  out_in_degree_ratio_list, clust_list, mean_path_len_list, betweenness_list = [], [], [], []
-		for sub in tqdm(range(GEC_ALL.shape[0])):
-			GEC = GEC_ALL[sub, :, :]
-			np.fill_diagonal(GEC, 0)
-			GEC = GEC.T
-			G = nx.from_numpy_array(GEC, create_using=nx.DiGraph())
+	out_in_degree_ratio_list, clust_list, mean_path_len_list, betweenness_list = [], [], [], []
+	for sub in tqdm(range(GEC_ALL.shape[0])):
+		GEC = GEC_ALL[sub, :, :]
+		np.fill_diagonal(GEC, 0)
+		GEC = GEC.T
+		G = nx.from_numpy_array(GEC, create_using=nx.DiGraph())
 
-			
-			# Degrees
-			indegree = np.array([G.in_degree(n, weight='weight') if G.is_directed() else G.degree(n, weight='weight') for n in G.nodes()])
-			indegree_list.append(indegree)
-			outdegree = np.array([G.out_degree(n, weight='weight') if G.is_directed() else G.degree(n, weight='weight') for n in G.nodes()])
-			outdegree_list.append(outdegree)
-			out_in_degree_ratio = np.divide(outdegree, indegree, out=np.zeros_like(outdegree), where=indegree!=0)
-			out_in_degree_ratio_list.append(out_in_degree_ratio)
+		# Degrees
+		indegree = np.array([G.in_degree(n, weight='weight') if G.is_directed() else G.degree(n, weight='weight') for n in G.nodes()])
+		indegree_list.append(indegree)
+		outdegree = np.array([G.out_degree(n, weight='weight') if G.is_directed() else G.degree(n, weight='weight') for n in G.nodes()])
+		outdegree_list.append(outdegree)
+		out_in_degree_ratio = np.divide(outdegree, indegree, out=np.zeros_like(outdegree), where=indegree!=0)
+		out_in_degree_ratio_list.append(out_in_degree_ratio)
 
-			# Clustering coefficient
-			clust = bct.clustering_coef_wd(GEC)
-			clust_list.append(clust)
+		# Clustering coefficient
+		clust = bct.clustering_coef_wd(GEC)
+		clust_list.append(clust)
 
-			# Path length
-			lengths = dict(nx.all_pairs_dijkstra_path_length(
-				G, 
-				weight=lambda u, v, d: 1/d['weight'] if d['weight'] > 0 else np.inf
-			))
-			mean_path_len = np.array([np.mean(list(lengths[i].values())) for i in range(len(GEC))])
-			mean_path_len_list.append(mean_path_len)
+		# Path length
+		lengths = dict(nx.all_pairs_dijkstra_path_length(
+			G, 
+			weight=lambda u, v, d: 1/d['weight'] if d['weight'] > 0 else np.inf
+		))
+		mean_path_len = np.array([np.mean(list(lengths[i].values())) for i in range(len(GEC))])
+		mean_path_len_list.append(mean_path_len)
 
-			# Betweenness centrality (works for directed networks)
-			betweenness_dict = nx.betweenness_centrality(G, weight='weight')
-			betweenness_array = np.array([betweenness_dict[n] for n in G.nodes()])
-			betweenness_list.append(betweenness_array)
+		# Betweenness centrality (works for directed networks)
+		betweenness_dict = nx.betweenness_centrality(G, weight='weight')
+		betweenness_array = np.array([betweenness_dict[n] for n in G.nodes()])
+		betweenness_list.append(betweenness_array)
 
-		out_in_degree_ratio_array_GEC = np.array(out_in_degree_ratio_list)
-		clust_array_GEC = np.array(clust_list)
-		mean_path_len_array_GEC = np.array(mean_path_len_list)
-		betweenness_array_GEC = np.array(betweenness_list)
+	out_in_degree_ratio_array_GEC = np.array(out_in_degree_ratio_list)
+	clust_array_GEC = np.array(clust_list)
+	mean_path_len_array_GEC = np.array(mean_path_len_list)
+	betweenness_array_GEC = np.array(betweenness_list)
 
-		metrics = {
-			'out_in_degree_ratio_array_GEC': out_in_degree_ratio_array_GEC,
-			'clust_array_GEC': clust_array_GEC,
-			'mean_path_len_array_GEC': mean_path_len_array_GEC,
-			'betweenness_array_GEC': betweenness_array_GEC
-		}
-		savemat(mat_path, metrics)
-		return metrics
+	metrics = {
+		'out_in_degree_ratio_array_GEC': out_in_degree_ratio_array_GEC,
+		'clust_array_GEC': clust_array_GEC,
+		'mean_path_len_array_GEC': mean_path_len_array_GEC,
+		'betweenness_array_GEC': betweenness_array_GEC
+	}
+	return metrics
 
 def regression_analysis(metrics, measure_array, metric_names, graph_type):
 	# Flatten metrics and hierarchical levels for regression
